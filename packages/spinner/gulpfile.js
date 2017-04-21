8c7e383443294fa33b07a585c439be30b6de895a
Sheng Di <SDi@walmartlabs.com>
2017-04-21 10:58:56 -0700

    Add eslint test in root level

--- packages/spinner/gulpfile.js
+++ packages/spinner/gulpfile.js
@@ -1,7 +1,5 @@
 "use strict";
 
-const exec = require("electrode-gulp-helper").exec;
-
 /**
  * There is a full range of gulp tasks defined in the above archetype
  * but below is a concise list of most often used gulp tasks at the
@@ -13,13 +11,7 @@ const exec = require("electrode-gulp-helper").exec;
  */
 
 const tasks = {
-  "demo": ["generate", "server-dev"],
-  "demo-iso": ["dev-iso"],
-  "generate": ["generate-metadata", "generate-documentation"],
-  "generate-documentation": () => exec(`electrode-docgen --package ./package.json --src ./src --markdown components.md`),
-  "generate-metadata": () => exec(`electrode-docgen --package ./package.json --src ./src --metadata components.json`),
-  "prepublish": ["npm:prepublish"],
-  "preversion": ["check-cov"]
+  "prepublish": ["npm:prepublish"]
 };
 
 require("electrode-archetype-react-component")(tasks);
